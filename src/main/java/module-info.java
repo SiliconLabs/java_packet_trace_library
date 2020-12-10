b92d256fe2da7506ead952456eb105d1b6640c24
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-12-10 09:00:21 -0400

    Remove the named module for now.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.silabs.pti {
-  exports com.silabs.pti;
-  exports com.silabs.pti.adapter;
-  exports com.silabs.pti.debugchannel;
-  exports com.silabs.pti.log;
-  exports com.silabs.pti.splitter;
-  exports com.silabs.pti.util;
-}

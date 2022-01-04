04f522811dabc533156b4a8c688242565bd6ed8f
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2022-01-04 10:03:54 -0500

    Comment out test options.

--- silabs-pti/src/main/java/com/silabs/pti/extcap/Extcap.java
+++ silabs-pti/src/main/java/com/silabs/pti/extcap/Extcap.java
@@ -19,7 +19,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.net.DatagramPacket;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -28,7 +27,6 @@ import java.util.Map;
 
 import com.silabs.pti.discovery.DiscoveryKey;
 import com.silabs.pti.discovery.DiscoveryUtil;
-import com.silabs.pti.discovery.IDiscoveryListener;
 
 /**
  * When this jar file is used within wireshark, the extcap wireshark
@@ -53,8 +51,8 @@ public class Extcap implements IExtcapInterface {
 
   private File logFile = null;
   private PrintWriter logWriter;
-  private PrintStream extcapOut;
-  private List<String> extcapArgs = new ArrayList<String>();
+  private final PrintStream extcapOut;
+  private final List<String> extcapArgs = new ArrayList<String>();
 
   /**
    * Execute extcap function. Args will contain 'extcap' as the first argument.
@@ -62,7 +60,7 @@ public class Extcap implements IExtcapInterface {
    * @param args
    * @return
    */
-  public static final int run(String[] args) {
+  public static final int run(final String[] args) {
     return new Extcap(args).run();
   }
 
@@ -71,8 +69,8 @@ public class Extcap implements IExtcapInterface {
    * 
    * @param args
    */
-  private Extcap(String[] args) {
-    String extcapLocation = System.getenv("EXTCAP_LOC");
+  private Extcap(final String[] args) {
+    final String extcapLocation = System.getenv("EXTCAP_LOC");
     if (extcapLocation != null) {
       logFile = new File(extcapLocation, "silabs-pti.log");
     }
@@ -85,32 +83,36 @@ public class Extcap implements IExtcapInterface {
   }
 
   /**
-   * This method logs to the log file, unrelated to wireshark, so you can debug what's happening.
+   * This method logs to the log file, unrelated to wireshark, so you can debug
+   * what's happening.
+   * 
    * @param s
    */
   @Override
-  public void log(String s) {
-    String d = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(new Date());
+  public void log(final String s) {
+    final String d = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(new Date());
     logWriter.println(d + ": " + s);
   }
 
   /**
    * This method prints output to the wireshark extcap communication protocol.
+   * 
    * @param s
    */
   @Override
-  public void extcapPrintln(String s) {
+  public void extcapPrintln(final String s) {
     log("extcap <           " + s);
     extcapOut.println(s);
   }
 
   /**
    * The method returns one of the toplevel commands for the extcap CLI protocol.
+   * 
    * @return command
    */
   private String extractCommandFromArgs() {
-    for (int i = 0; i < extcapArgs.size(); i++) {
-      switch (extcapArgs.get(i)) {
+    for (final String extcapArg : extcapArgs) {
+      switch (extcapArg) {
       case EC_INTERFACES:
         return EC_INTERFACES;
       case EC_DLTS:
@@ -126,12 +128,13 @@ public class Extcap implements IExtcapInterface {
   }
 
   /**
-   * Given an argument, this returns the value of the argument, so essentially the next argument.
+   * Given an argument, this returns the value of the argument, so essentially the
+   * next argument.
    * 
    * @param arg
    * @return value of a given argument.
    */
-  private String extractValueFromArg(String arg) {
+  private String extractValueFromArg(final String arg) {
     for (int i = 0; i < extcapArgs.size(); i++) {
       if (extcapArgs.get(i).equals(arg)) {
         if (i + 1 < extcapArgs.size()) {
@@ -151,11 +154,11 @@ public class Extcap implements IExtcapInterface {
     try (PrintWriter l = (logFile != null ? new PrintWriter(new FileWriter(logFile, true), true)
         : new PrintWriter(System.out))) {
       logWriter = l;
-      StringBuilder sb = new StringBuilder("extcap > ");
-      for (String s : extcapArgs)
+      final StringBuilder sb = new StringBuilder("extcap > ");
+      for (final String s : extcapArgs)
         sb.append(" ").append(s);
       log(sb.toString());
-      String cmd = extractCommandFromArgs();
+      final String cmd = extractCommandFromArgs();
       if (cmd != null) {
         switch (cmd) {
         case EC_INTERFACES:
@@ -170,7 +173,7 @@ public class Extcap implements IExtcapInterface {
       } else {
         return 1;
       }
-    } catch (Exception e) {
+    } catch (final Exception e) {
       System.err.println("Error: " + e.getMessage());
       e.printStackTrace();
     }
@@ -179,35 +182,34 @@ public class Extcap implements IExtcapInterface {
 
   /**
    * Entry point for the interfaces list command.
+   * 
    * @return program return code
    */
   private int extcapInterfaces() {
     extcapPrintln("extcap {version=1.0}{help=http://silabs.com}");
-    DiscoveryUtil.runDiscovery(new IDiscoveryListener() {
-      @Override
-      public void discovered(DatagramPacket packet) {
-        Map<DiscoveryKey, String> data = DiscoveryUtil.parseDiscoveryMap(packet);
-        String name = "Silicon Labs WSTK adapter";
-        String ip = packet.getAddress().getHostAddress();
-        if ( data.containsKey(DiscoveryKey.ADAPTER_NETIF)) {
-          ip = data.get(DiscoveryKey.ADAPTER_NETIF);
-        }
-
-        if ( data.containsKey(DiscoveryKey.ADAPTER_NICKNAME)) {
-          name = data.get(DiscoveryKey.ADAPTER_NICKNAME) + " (Silicon Labs WSTK adapter)";
-        }
-        
-        extcapPrintln("interface {value=" + ip + "}{display=" + name + "}");
+    DiscoveryUtil.runDiscovery(packet -> {
+      final Map<DiscoveryKey, String> data = DiscoveryUtil.parseDiscoveryMap(packet);
+      String name = "Silicon Labs WSTK adapter";
+      String ip = packet.getAddress().getHostAddress();
+      if (data.containsKey(DiscoveryKey.ADAPTER_NETIF)) {
+        ip = data.get(DiscoveryKey.ADAPTER_NETIF);
       }
+
+      if (data.containsKey(DiscoveryKey.ADAPTER_NICKNAME)) {
+        name = data.get(DiscoveryKey.ADAPTER_NICKNAME) + " (Silicon Labs WSTK adapter)";
+      }
+
+      extcapPrintln("interface {value=" + ip + "}{display=" + name + "}");
     });
     return 0;
   }
 
   /**
    * Entry point for the DLTs list.
+   * 
    * @return program return code
    */
-  private int extcapDlts(String ifc) {
+  private int extcapDlts(final String ifc) {
     if (ifc == null)
       return 1;
     extcapPrintln("dlt {number=147}{name=USER1}{display=WSTK Silicon Labs DLT}");
@@ -216,38 +218,40 @@ public class Extcap implements IExtcapInterface {
 
   /**
    * Entry point for the capture initialization.
+   * 
    * @return program return code
    */
-  private int extcapCapture(String ifc) {
+  private int extcapCapture(final String ifc) {
     if (ifc == null)
       return 1;
-    String fifo = extractValueFromArg(EC_FIFO);
-    String filter = extractValueFromArg(EC_CAPTURE_FILTER);
+    final String fifo = extractValueFromArg(EC_FIFO);
+    final String filter = extractValueFromArg(EC_CAPTURE_FILTER);
     log("capture: from " + ifc + " into " + fifo + (filter == null ? " with no filter" : (" with filter " + filter)));
-    ExtcapCapture capture = new ExtcapCapture(ifc, fifo, filter);
+    final ExtcapCapture capture = new ExtcapCapture(ifc, fifo, filter);
     try {
       capture.capture(this);
       return 0;
-    } catch (IOException ioe) {
+    } catch (final IOException ioe) {
       log("error during capture: " + ioe.getMessage());
-      return 1;      
+      return 1;
     }
   }
 
   /**
    * Entry point for the configuration information.
+   * 
    * @return program return code
    */
-  private int extcapConfig(String ifc) {
+  private int extcapConfig(final String ifc) {
     if (ifc == null)
       return 1;
-    extcapPrintln("arg {number=0}{call=--delay}{display=WSTK delay}{tooltip=Time delay between packages}{type=integer}{range=1,15}{required=true}");
-    extcapPrintln("arg {number=1}{call=--message}{display=WSTK Message}{tooltip=Package message content}{placeholder=Please enter a message here ...}{type=string}");
-    extcapPrintln("arg {number=2}{call=--verify}{display=WSTK Verify}{tooltip=Verify package content}{type=boolflag}");
-    extcapPrintln("arg {number=3}{call=--remote}{display=WSTK Remote Channel}{tooltip=Remote Channel Selector}{type=selector}");
-    extcapPrintln("arg {number=4}{call=--server}{display=WSTK IP address for log server}{type=string}{validation=\\\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\\\b}");
-    extcapPrintln("value {arg=3}{value=if1}{display=WSTK Remote1}{default=true}");
-    extcapPrintln("value {arg=3}{value=if2}{display=WSTK Remote2}{default=false}");
+//    extcapPrintln("arg {number=0}{call=--delay}{display=WSTK delay}{tooltip=Time delay between packages}{type=integer}{range=1,15}{required=true}");
+//    extcapPrintln("arg {number=1}{call=--message}{display=WSTK Message}{tooltip=Package message content}{placeholder=Please enter a message here ...}{type=string}");
+//    extcapPrintln("arg {number=2}{call=--verify}{display=WSTK Verify}{tooltip=Verify package content}{type=boolflag}");
+//    extcapPrintln("arg {number=3}{call=--remote}{display=WSTK Remote Channel}{tooltip=Remote Channel Selector}{type=selector}");
+//    extcapPrintln("arg {number=4}{call=--server}{display=WSTK IP address for log server}{type=string}{validation=\\\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\\\b}");
+//    extcapPrintln("value {arg=3}{value=if1}{display=WSTK Remote1}{default=true}");
+//    extcapPrintln("value {arg=3}{value=if2}{display=WSTK Remote2}{default=false}");
     return 0;
   }
 }

1454bdbbc5177e1a3a91b82f5687346bfd67b36a
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2022-04-18 16:19:49 -0400

    Move a window interface to the library.

---
+++ silabs-pti/src/main/java/com/silabs/pti/decode/IAemCurrentSampleWindow.java
@@ -0,0 +1,34 @@
+// Copyright (c) 2017 Silicon Labs. All rights reserved.
+
+package com.silabs.pti.decode;
+
+import java.util.stream.Stream;
+
+/**
+ * A window of sampler retrieved from resampling.
+ *
+ * @author Timotej
+ * Created on Nov 16, 2017
+ */
+public interface IAemCurrentSampleWindow {
+
+  /**
+   * Returns the count of all samples.
+   * @return
+   */
+  public int size();
+
+  /**
+   * Returns a full information of i-th sample.
+   * @param i
+   * @return
+   */
+  public IAemCurrentSample sample(int i);
+
+  /**
+   * Returns the stream of samples.
+   * @return stream.
+   */
+  public Stream<IAemCurrentSample> stream();
+
+}

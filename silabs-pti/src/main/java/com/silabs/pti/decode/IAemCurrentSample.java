5d60f779975c280c6fc98a4deedd6524d1923180
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2022-04-18 15:01:07 -0400

    Add the current sampler interface.

---
+++ silabs-pti/src/main/java/com/silabs/pti/decode/IAemCurrentSample.java
@@ -0,0 +1,28 @@
+// Copyright (c) 2022 Silicon Labs. All rights reserved.
+
+package com.silabs.pti.decode;
+
+/**
+ * Interface that represents a single current sample.
+ *
+ * @author timotej
+ * Created on Apr 18, 2022
+ */
+public interface IAemCurrentSample {
+
+  /**
+   * Timestamp in microseconds
+   */
+  public long timestamp();
+
+  /**
+   * Current value
+   */
+  public float current();
+
+  /**
+   * Voltage value
+   */
+  public float voltage();
+
+}

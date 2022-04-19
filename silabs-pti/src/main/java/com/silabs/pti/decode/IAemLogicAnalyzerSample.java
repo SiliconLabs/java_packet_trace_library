ca89ca8478ab13aeef004e5ae8826f4cc3e53961
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2022-04-19 11:17:39 -0400

    Add the interfaces and basic classes to bring logic analyzer capabilities into the library.

---
+++ silabs-pti/src/main/java/com/silabs/pti/decode/IAemLogicAnalyzerSample.java
@@ -0,0 +1,20 @@
+// Copyright (c) 2022 Silicon Labs. All rights reserved.
+
+package com.silabs.pti.decode;
+
+/**
+ * Single sample from a logic analyzer.
+ *
+ * @author timotej
+ * Created on Apr 19, 2022
+ */
+public interface IAemLogicAnalyzerSample extends IAemTimedSample {
+
+  /**
+   * Returns the full state of the channels.
+   * Least significant bit, is by convention called "channel zero".
+   *
+   * @return state of channels, one bit per channel.
+   */
+  public int channelState();
+}
---
+++ silabs-pti/src/main/java/com/silabs/pti/decode/LogicAnalyzerSample.java
@@ -0,0 +1,30 @@
+// Copyright (c) 2022 Silicon Labs. All rights reserved.
+
+package com.silabs.pti.decode;
+
+/**
+ * Single class representing the state of the logic analyzer.
+ *
+ * @author timotej
+ * Created on Apr 19, 2022
+ */
+public class LogicAnalyzerSample implements IAemLogicAnalyzerSample {
+
+  private final long t;
+  private final int channelState;
+
+  public LogicAnalyzerSample(final long t, final int channelState) {
+    this.t = t;
+    this.channelState = channelState;
+  }
+
+  @Override
+  public long timestamp() {
+    return t;
+  }
+
+  @Override
+  public int channelState() {
+    return channelState;
+  }
+}

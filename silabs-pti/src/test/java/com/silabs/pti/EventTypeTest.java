4a7fa4a55ee37cf1b2d3408a2652aa29148639d9
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2023-03-20 14:42:40 -0400

    Update PCAP to latest. (#43)
    * Update PCAP to latest.
    
    * Upgrade to java 19 for default build.
    
    * Revert "Upgrade to java 19 for default build."
    
    This reverts commit 74a2681e19fc297c4151a72579cf09523109a8e7.
    
    * Revert to public build with java 11.

--- silabs-pti/src/test/java/com/silabs/pti/EventTypeTest.java
+++ silabs-pti/src/test/java/com/silabs/pti/EventTypeTest.java
@@ -2,10 +2,12 @@
 
 package com.silabs.pti;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
+import com.silabs.na.pcap.LinkType;
 import com.silabs.pti.debugchannel.EventType;
 
 class EventTypeTest {
@@ -13,6 +15,10 @@ class EventTypeTest {
   @Test void eventTypes() {
     EventType et = EventType.ASSERT;
     assertNotNull(et);
+  }
+
+  @Test void dlts() {
+    assertEquals(LinkType.SILABS_DEBUG_CHANNEL, LinkType.silabsDebugChannel());
   }
 
 }
